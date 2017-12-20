e0502e21fe6ce2ad972e76e4ebb32493aeeee509
Jake Deichert <git@jakedeichert.com>
2017-12-20 16:35:24 -0500

    Organize more

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,27 @@
 const WASM_ASTAR = {
-  wasmModulePath: 'wasm_astar.wasm',
   renderManager: null,
-  debug: true,
+  wasmModulePath: 'wasm_astar.wasm',
+  width: 900,
+  height: 600,
+  debug: true, // Wasm converts to an int
   renderIntervalMs: 10000, // Used in debug mode
+};
+
+const init = () => {
+  const { wasmModulePath, width, height, debug, renderIntervalMs } = WASM_ASTAR;
+  const renderManager = new RenderManager(width, height);
+  WASM_ASTAR.renderManager = renderManager;
+  const wasmImports = {
+    js_request_tick: renderManager.requestNextTick,
+    js_start_interval_tick: renderManager.startIntervalTick,
+    js_clear_screen: renderManager.clearScreen,
+    js_update: renderManager.update,
+    js_draw_tile: renderManager.drawTile,
+  };
+  return loadWasm(wasmModulePath, wasmImports).then(wasmModule => {
+    renderManager.setWasmModuleTicker(wasmModule.tick);
+    wasmModule.init(debug, renderIntervalMs);
+  });
 };
 
 class CanvasRenderer {
@@ -32,10 +51,8 @@ class CanvasRenderer {
 }
 
 class RenderManager {
-  constructor() {
+  constructor(width, height) {
     this.bindMethods(this);
-    const width = 900;
-    const height = 600;
     this.isIntervalTick = false;
     this.wasmModuleTick = () => {};
     // Could have multiple canvas renderers (background, foreground) and render
@@ -100,20 +117,4 @@ const loadWasm = (filepath, wasmImports) => {
     });
 };
 
-const init = () => {
-  WASM_ASTAR.renderManager = new RenderManager();
-  const wasmImports = {
-    js_request_tick: WASM_ASTAR.renderManager.requestNextTick,
-    js_start_interval_tick: WASM_ASTAR.renderManager.startIntervalTick,
-    js_clear_screen: WASM_ASTAR.renderManager.clearScreen,
-    js_update: WASM_ASTAR.renderManager.update,
-    js_draw_tile: WASM_ASTAR.renderManager.drawTile,
-  };
-  return loadWasm(WASM_ASTAR.wasmModulePath, wasmImports).then(wasmModule => {
-    WASM_ASTAR.renderManager.setWasmModuleTicker(wasmModule.tick);
-    const debug = WASM_ASTAR.debug ? 1 : 0;
-    wasmModule.init(debug, WASM_ASTAR.renderIntervalMs);
-  });
-};
-
-window.addEventListener('load', () => init());
+window.addEventListener('load', init);
